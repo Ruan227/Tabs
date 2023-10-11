e7d4941076f361acb1f0688666cbb7f3f7ea981a
Ruan227 <ruansilva.pascoal@gmal.com>
2023-10-11 20:16:54 -0300

    first commit

---
+++ src/screens/users/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import { Avatar, Card, IconButton } from 'react-native-paper'
+import { FlatList, ScrollView, Text, View, StyleSheet } from 'react-native';
+import Api from '../../services/Api'
+
+const Users = () => {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+      Api.get('users')
+        .then((response) => {
+          setUsers(response.data.users)
+        }).catch((error) => {
+          console.log(error)
+        })
+    }, [])
+
+  return (
+    <>
+        <ScrollView>
+        {users.map((item) => (
+             <Card key={item.id} style={styles.card}>
+                <Card.Title
+                    title={item.username}
+                    subtitle={item.email}
+                    left={() => <Avatar.Image size={48} source={{ uri: item.image }} />}
+                />
+            </Card>
+        ))}
+        </ScrollView>
+    </>
+    
+
+
+  )
+}
+
+export default Users
+
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: 25 
+  }
+})
